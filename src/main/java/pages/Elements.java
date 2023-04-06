2dc6344d8907044f4b9172543aabcfceab56e245
automation-craftsman <aliulislamabir@gmail.com>
2023-04-06 18:08:02 +0600

    Removed modules from the project structure.

--- src/main/java/pages/Elements.java
+++ src/main/java/pages/Elements.java
@@ -1,4 +1,4 @@
-package modules.elements;
+package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -9,10 +9,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class TextBox {
+import static org.testng.AssertJUnit.assertTrue;
+
+public class Elements {
 
     private WebDriver driver;
 
+    // Text Box related fields
     private By textbox = By.xpath("/html/body/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/ul/li[1]");
     private By name_field = By.xpath("//*[@id=\"userName\"]");
     private By email_field = By.xpath("//*[@id=\"userEmail\"]");
@@ -22,10 +25,17 @@ public class TextBox {
 
     private By output_box_name = By.xpath("//*[@id=\"name\"]");
 
-    public TextBox(WebDriver driver){
+    // Check Box related fields
+    private By check_box = By.xpath("/html/body/div[2]/div/div/div[2]/div[1]/div/div/div[1]/div/ul/li[2]/span");
+    private By expand_all = By.cssSelector("#tree-node > div > button.rct-option.rct-option-expand-all > svg");
+
+
+    // Constructor
+    public Elements(WebDriver driver){
         this.driver = driver;
     }
 
+    // Text Box related methods
     public void getTextBox(){
         getElementIntoView(textbox);
         driver.findElement(textbox).click();
@@ -51,13 +61,39 @@ public class TextBox {
         driver.findElement(submit_button).click();
     }
 
-    public String verifyOutput(){
+    public String verifyTextBoxOutput(){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         wait.until(ExpectedConditions.visibilityOf(driver.findElement(output_box_name)));
 
         return driver.findElement(output_box_name).getText();
     }
 
+    // Checkbox related methods
+    public void getCheckBox(){
+        getElementIntoView(check_box);
+        driver.findElement(check_box).click();
+    }
+
+    public void expandAll(){
+        driver.findElement(expand_all).click();
+    }
+
+    public void checkTheBox(By element){
+        getElementIntoView(element);
+        driver.findElement(element).click();
+    }
+
+    public void verifyCheckBoxOutput(By output_container_element, String expected_output){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(output_container_element)));
+
+        String output_text = driver.findElement(output_container_element).getText();
+
+        assertTrue(output_text.contains(expected_output));
+    }
+
+
+    // Private helper methods
     private void getElementIntoView(By element){
         WebElement target = driver.findElement(element);
         ((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();",target);
